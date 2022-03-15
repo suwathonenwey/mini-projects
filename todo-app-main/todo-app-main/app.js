89b571e5e29e23f9db086d205d8a3208a9ed5cef
suwathonenwey <92215947+suwathonenwey@users.noreply.github.com>
2022-03-15 07:55:08 +0630

    fourth

--- todo-app-main/todo-app-main/app.js
+++ todo-app-main/todo-app-main/app.js
@@ -2,6 +2,7 @@ let input = document.querySelector("#todo-input");
 let todoUL = document.querySelector("#todo-ul");
 let number = document.querySelector("#number");
 let allClear = document.querySelector(".clear-completed");
+let choices = document.querySelectorAll(".choice");
 
 
 //add event listener
@@ -9,43 +10,56 @@ input.addEventListener("keyup", addToDo);
 todoUL.addEventListener("click", deleteAndComplete);
 allClear.addEventListener("click", clearAll);
 
+
 //functions
+
+function createList(){
+    if(input.value.trim()){
+        //create elements and append
+    let todoLI = document.createElement("li");
+    let innerDiv = document.createElement("div");
+    let todoCircle = document.createElement("a");
+
+    let todoContent = document.createElement("p");
+    todoContent.textContent = input.value;
+
+    let todoDelete = document.createElement("a");
+    let img = document.createElement("img");
+    img.src = `images/icon-cross.svg`;
+    todoDelete.append(img);
+
+
+    innerDiv.appendChild(todoCircle);
+    innerDiv.appendChild(todoContent);
+
+    todoLI.appendChild(innerDiv);
+    todoLI.appendChild(todoDelete);
+
+
+    //add classes to newly created elements
+    todoLI.classList.add("todo__item");
+    innerDiv.classList.add("todo__inner");
+    todoCircle.classList.add("todo__circle");
+    todoContent.classList.add("todo__content");
+    todoDelete.classList.add("cancle");
+
+    //append to ul
+    todoUL.append(todoLI);
+
+    input.value = "";
+    localStorage.setItem("todo-list", todoUL.innerHTML)
+    }
+}
+
 function addToDo(e){
     if(e.keyCode == "13"){
-        if(input.value.trim()){
-                    //create elements and append
-        let todoLI = document.createElement("li");
-        let innerDiv = document.createElement("div");
-        let todoCircle = document.createElement("a");
-
-        let todoContent = document.createElement("p");
-        todoContent.textContent = input.value;
-
-        let todoDelete = document.createElement("a");
-        let img = document.createElement("img");
-        img.src = `images/icon-cross.svg`;
-        todoDelete.append(img);
-
-
-        innerDiv.appendChild(todoCircle);
-        innerDiv.appendChild(todoContent);
-
-        todoLI.appendChild(innerDiv);
-        todoLI.appendChild(todoDelete);
-
-
-        //add classes to newly created elements
-        todoLI.classList.add("todo__item");
-        innerDiv.classList.add("todo__inner");
-        todoCircle.classList.add("todo__circle");
-        todoContent.classList.add("todo__content");
-        todoDelete.classList.add("cancle");
-
-        //append to ul
-        todoUL.append(todoLI);
-
-        input.value = "";
+        
+        let noText = document.querySelector(".no-text");
+        if(noText){
+            noText.remove();
         }
+
+        createList();
 
     }
 }
@@ -70,5 +84,112 @@ function deleteAndComplete(e){
 function clearAll(todoUL){
 
     todoUL.innerHTML = "";
+    localStorage.removeItem("todo-list");
+}
+
+function filterTasks(e){
+    console.log(e.target);
+
+    //loop choices and filter tasks
+    choices.forEach(choice => {
+        choice.addEventListener("click", function(){
+            let choiceId = choice.getAttribute("id");
+            
+            switch(choiceId){
+                case "all":
+                    break;
+
+                    case "ac-text":
+                        console.log("ac-text");
+                        todoUL.removeChild();
+                        break;
+
+                    case "completed":
+                        console.log("completed")
+            }
+
+        })
+    });
+}
+
+function storeInLocalStorage(){
+    if(localStorage.getItem("todo-list")){
+        todoUL.innerHTML = localStorage.getItem("todo-list");
+        console.log("there is list");
+    }else{
+        todoUL.innerHTML = `<h3 class="no-text">There is no task yet. </h3>`;
+        console.log("there is no list")
+    }
+}
+
+storeInLocalStorage();
+
+
+choices.forEach(choice => {
+    choice.addEventListener("click", function(){
+        let choiceId = choice.getAttribute("id");
+        
+        switch(choiceId){
+            case "all":
+                break;
+
+                case "ac-text":
+                    console.log("ac-text");
+                    
+                    break;
+
+                case "completed":
+                    console.log("completed")
+        }
+
+    })
+});
+
+// Array.from(choices).forEach(function(choice){
+//     choice.addEventListener("click", function(e){
+//         let list = e.target.parentElement.parentElement.nextElementSibling;
+//         // let listItem = list.children;
+//         console.log(list)
+//         console.log(listItem)
+//     })
+// })
+
+for(let i = 0; i < todoUL.children.length; i ++){
+
+    choices.forEach(choice => {
+        choice.addEventListener("click", function(){
+            let choiceId = choice.getAttribute("id");
+            let task = todoUL.children[i].firstChild.firstChild;
+            
+            
+            switch(choiceId){
+                case "all":
+                    task.parentElement.parentElement.style.display = "flex";
+                    break;
+    
+                    case "ac-text":
+                        if(task.classList.contains("todo__complete")){
+                            task.parentElement.parentElement.style.display = "none";
+                        }else{
+                            task.parentElement.parentElement.style.display = "flex";
+                        }
+                        break;
+    
+                    case "completed":
+                        console.log("completed")
+
+                        if(task.classList.contains("todo__complete")){
+                            task.parentElement.parentElement.style.display = "flex";
+                        }else{
+                            task.parentElement.parentElement.style.display = "none";
+                        }
+                        break;
+            }
+    
+        })
+    });
+}
+
+function clearAll(){
 
 }

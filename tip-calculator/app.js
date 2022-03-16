03b01dd86e193c034364942c3de36bd069332a2f
suwathonenwey <92215947+suwathonenwey@users.noreply.github.com>
2022-03-16 18:16:09 +0630

    not responsive yet

--- tip-calculator/app.js
+++ tip-calculator/app.js
@@ -6,28 +6,14 @@ const percentTip = document.querySelectorAll("#percent-tip");
 let tipText = document.querySelector(".tip");
 let totalText = document.querySelector(".total");
 let resetBtn = document.querySelector("#reset");
+let zero = document.querySelector("#zero");
 
 
 for(const tip of percentTip){
     tip.addEventListener("click", function(e){
         e.preventDefault();
         let percentValue = tip.getAttribute("data-value");
-
-        //get total tip
-        let totalTip = (toNumber(billInput.value) / 100) * percentValue;
-
-        //get total bill
-        let totalBill = toNumber(billInput.value) + totalTip;
-
-        //person per tip
-        let personPerTip = totalTip / personInput.value;
-
-        //person per total 
-        let personPerTotal = totalBill / personInput.value;
-
-        //put value into innerhtml
-        tipText.innerHTML = personPerTip.toFixed(2);
-        totalText.innerHTML = personPerTotal.toFixed(2);
+        calculate(percentValue);
     })
 }
 
@@ -36,25 +22,45 @@ customInput.addEventListener("keyup", function(e){
     e.preventDefault();
     console.log(customInput.value);
     let percentValue = customInput.value;
-        //get total tip
-        let totalTip = (toNumber(billInput.value) / 100) * percentValue;
+    calculate(percentValue);
 
-        //get total bill
-        let totalBill = toNumber(billInput.value) + totalTip;
+});
 
-        //person per tip
-        let personPerTip = totalTip / personInput.value;
-
-        //person per total 
-        let personPerTotal = totalBill / personInput.value;
-
-        //put value into innerhtml
-        tipText.innerHTML = personPerTip.toFixed(2);
-        totalText.innerHTML = personPerTotal.toFixed(2);
+resetBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    billInput.value = "";
+    customInput.value = "";
+    personInput.value = "";
+    tipText.innerHTML = "0.00";
+    totalText.innerHTML = "0.00";
 })
 
 //functions
 function toNumber(x){
     return Number(x);
-}
+};
 
+function calculate(x){
+    if(personInput.value == 0){
+        zero.classList.add("zero");
+        zero.querySelector(".zero-text").style.display = "block";
+        zero.nextSibling().style.display = "block";
+    }
+    else{
+                        //get total tip
+                        let totalTip = (toNumber(billInput.value) / 100) * x;
+
+                        //get total bill
+                        let totalBill = toNumber(billInput.value) + totalTip;
+                
+                        //person per tip
+                        let personPerTip = totalTip / personInput.value;
+                
+                        //person per total 
+                        let personPerTotal = totalBill / personInput.value;
+                
+                        //put value into innerhtml
+                        tipText.innerHTML = personPerTip.toFixed(2);
+                        totalText.innerHTML = personPerTotal.toFixed(2);
+    }
+}

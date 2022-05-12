6de8ef403b52706dbfbbf4961422855af4cce667
suwathonenwey <92215947+suwathonenwey@users.noreply.github.com>
2022-05-12 20:46:01 +0630

    responsive

--- intro-section-with-dropdown-navigation-main/app.js
+++ intro-section-with-dropdown-navigation-main/app.js
@@ -1,5 +1,8 @@
 let featureBtn = document.getElementById("feature");
 let companyBtn = document.getElementById("company");
+let menuIcon = document.querySelector(".menu-icon");
+let closeIcon = document.querySelector(".close-icon");
+let sidebar = document.getElementById("sidebar");
 
 featureBtn.addEventListener("click", () => {
     console.log("feature")
@@ -9,3 +12,11 @@ companyBtn.addEventListener("click", () => {
     console.log("company")
     document.getElementById("company-dropdown").classList.toggle("block");
 });
+menuIcon.addEventListener("click", () => {
+    sidebar.style.marginRight = "0";
+    document.getElementById("sidebar-bg").classList.toggle("block")
+});
+closeIcon.addEventListener("click", () => {
+    sidebar.style.marginRight = "-100%";
+    document.getElementById("sidebar-bg").classList.toggle("block")
+})
